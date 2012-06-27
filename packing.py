a7e6cac6c9a7c5914d780ce02b456c5a391e31bc
Chris McGraw <chris.mcgraw@lodgenet.com>
2012-06-27 12:56:20 -0500

    Adds basic template

--- packing.py
+++ packing.py
@@ -1,9 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 
 application = Flask(__name__)
 application.debug = True
 
-@application.route('/')
-def index():
-    return "Sup G"
 
+@application.route('/')
+@application.route('/<beans>')
+def index(beans=None):
+    return render_template('index.html', beans=beans)
+
+if __name__ == '__main__':
+    application.run(debug=True)
